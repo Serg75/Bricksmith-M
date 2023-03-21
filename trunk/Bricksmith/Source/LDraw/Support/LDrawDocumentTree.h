7714f4812f12dd6f738ad595b3fc91d10a7297a9
Sergey Slobodenyuk <sergn75@gmail.com>
2023-03-21 22:36:45 +0100

    Fixed and improved duplicate and paste commands: - Preventing to paste different types of directives (models, steps, parts...) at the same time - Paste command inserts part duplicates next to originals

--- trunk/Bricksmith/Source/LDraw/Support/LDrawDocumentTree.h
+++ trunk/Bricksmith/Source/LDraw/Support/LDrawDocumentTree.h
@@ -21,6 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface LDrawDocumentTree : NSObject
 
 + (NSArray *)mostInnerDirectives:(NSArray *)objects;
++ (LDrawDirective *)similarDirective:(LDrawDirective *)directive amongObjects:(id)objects;
 + (NSSet<NSString *> *) groupsBeforeStep:(LDrawStep *)step;
 
 @end
